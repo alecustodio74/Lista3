3bd6c2e2836b30fe60b794947efddcced17c0a37
Alexandre Custódio <arcs1234@hotmail.com>
2025-03-04 15:51:09 -0300

    Lista 3 completa

---
+++ 8_resposta.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contagem regressiva</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h4>Resposta do exercício 8</h4>
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            try{
+            $numero = $_POST['num'];
+            $i = $numero;
+            do{
+                echo " $i ";
+                $i--;
+            }while($i >= 1);
+            
+            } catch(Exception $e){
+                echo $e->getMessage();
+            }
+        }
+    ?>
+    <div class="col mt-2">
+         <button type="submit" class="btn btn-primary"><a href="8_regressiva.php" style="color: white; text-decoration: none;">Voltar</a></button>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
